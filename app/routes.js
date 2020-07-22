f50f7e3d52f0986e9451625825063f4a71552e39
Miles-Cheverton <36925450+Miles-Cheverton@users.noreply.github.com>
2020-07-22 11:25:20 +0100

    Updates

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+	//xxxxxx
+	router.get('/admin/choice_action', function (req, res) {
+		
+		var choice = req.session.data['choice'];
+
+		if (choice =='content') {
+			res.render('admin/new_content', {})
+		}
+		else if (choice =='news') {
+			res.render('admin/new_news', {})
+		}
+	})
+
 module.exports = router
