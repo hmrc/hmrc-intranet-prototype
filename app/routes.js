123c2a8fd1943ac5222eac3f135d04449548b596
Miles-Cheverton <36925450+Miles-Cheverton@users.noreply.github.com>
2020-07-27 09:51:22 +0100

    added publish journey and other misc changes

--- app/routes.js
+++ app/routes.js
@@ -8,12 +8,29 @@ const router = express.Router()
 		
 		var choice = req.session.data['choice'];
 
-		if (choice =='content') {
-			res.render('admin/new_content', {})
-		}
-		else if (choice =='news') {
+		if (choice =='news') {
 			res.render('admin/new_news', {})
+		}
+		if (choice =='guidance') {
+			res.render('admin/new_guidance', {})
+		}
+		else if (choice =='policy') {
+			res.render('admin/new_policy', {})
 		}
 	})
 
+	//Route to edit or published depending on choice
+	router.get('/admin/final_publish_choice_action', function (req, res) {
+		
+		var choice = req.session.data['choice'];
+
+		if (choice =='publish') {
+			res.render('admin/sent_to_publish_confirm', {})
+		}
+		else if (choice =='edit') {
+			res.render('admin/edit_news', {})
+		}
+	})
+	
+	
 module.exports = router
